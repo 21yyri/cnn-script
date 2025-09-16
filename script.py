e8654937e6545b116025ee8d8416d9e325512afc
Yuri T. <155418226+21yyri@users.noreply.github.com>
2025-09-16 17:08:27 -0300

    init commit

---
+++ script.py
@@ -0,0 +1,72 @@
+import feedparser, requests, os, pytz
+from dotenv import load_dotenv
+from datetime import datetime
+from bs4 import BeautifulSoup
+from time import mktime
+
+def format_categoria(categoria: str) -> str:
+    """Retorna a categoria com capitalização e acentuação."""
+    match categoria:
+        case 'politica':
+            return 'Política'
+        case 'educacao':
+            return 'Educação'
+        case 'saude':
+            return 'Saúde'
+        case _:
+            return categoria.capitalize()
+
+
+def get_imagem(url: str) -> str:
+    """Recebe uma notícia e retorna a URL para a imagem de capa da notícia."""
+    html = requests.get(url)
+    soup = BeautifulSoup(html.text, 'html.parser')
+
+    imagem = soup.select('img.flex.size-full.object-cover')[2]
+    return imagem.get("src")
+
+
+load_dotenv()
+
+COMUNICA_URL = "http://127.0.0.1:8000/api"
+
+RSS_URL = "https://admin.cnnbrasil.com.br/feed/"
+feed = feedparser.parse(RSS_URL)
+
+if feed.status != 200:
+    raise requests.exceptions.HTTPError()
+
+CATEGORIAS = [
+    'economia', 'politica', 
+    'esportes', 'educacao', 
+    'nacional', 'saude',
+    'tecnologia', 'nacional'
+]
+
+for entry in feed.entries:
+    categoria = entry.link.split('/')[3]
+    if categoria not in CATEGORIAS:
+        continue
+    
+    noticia = {
+        "autor": entry.author,
+        "setor": format_categoria(categoria),
+        "titulo": entry.title,
+        "body": entry.summary + '.',
+        "link": entry.link,
+        "data": str(
+            datetime.fromtimestamp(mktime(entry.published_parsed), tz = pytz.UTC)
+        ),
+        "imagem": get_imagem(entry.link)
+    }
+
+    token = requests.post(f"{COMUNICA_URL}/login/", json = {
+        "username": os.getenv("USERNAME"),
+        "password": os.getenv("PASSWORD")
+    }).json()
+    
+    response = requests.post(f"{COMUNICA_URL}/news/post/", json = noticia, headers = {
+        "Authorization": f"Token {token.get("Token")}"
+    })
+
+    print(response.status_code)
